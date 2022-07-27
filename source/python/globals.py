cb940ffe0660fc6a9758a2363a01b12661e729a3
Weiyi Xia <xwy0220@gmail.com>
2022-07-27 10:25:51 -0500

    initial commit

---
+++ source/python/globals.py
@@ -0,0 +1,11 @@
+
+BEARER_TOKEN_YAML_PATH = "/Users/someone/.twitter_keys.yaml"
+DB_NAME = 'db_name'
+DB_USER_NAME = 'someone'
+TWEETS_TABLE_NAME = 'tname'
+REPLIES_TABLE_NAME = 'tname_conversations'
+START_DATE ='2022-03-01'
+END_DATE = '2022-04-01'
+SLEEP_TIME = 60
+QUERY_FILE = 'filteringrule.txt'
+MAX_RESULTS = 50
